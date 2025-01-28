78088f8a4bc363c337b196bd9827b2d0ebac3324
Dax-savani <dakshsavani15@gmail.com>
2025-01-28 09:23:13 +0530

    first commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,15 +4,12 @@ const User = require('../models/user')
 async function auth(req, res, next) {
 
     let authToken = req.cookie;
-    console.log("authToken:", authToken);
     if (!authToken) return res.status(401).json({message: "UnAuthorised: Auth token not found!", status: 401});
 
     if (authToken.toLowerCase().startsWith("bearer ")) {
         authToken = authToken.split(" ")[1];
     }
-
     const user = await verifyToken(authToken);
-    console.log(user);
     const verifiedUser = await User.findById(user.id);
 
     if (!verifiedUser) return res.status(401).json({message: "UnAuthorised: Auth token is invalid!", status: 401});
