95886fbb3323b658014d4ad6a8208118f1d48c60
Dax-savani <dakshsavani15@gmail.com>
2025-01-29 09:26:33 +0530

    initial commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const corsOptions = {
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
